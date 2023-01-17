73ceef5a5c66870e6f6f850f61d5af7005126d56
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-01-17 15:12:38 +0530

    first commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bloodbank_app/main.dart';
+import 'package:bloodbank_app/screen/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
